70d388bd426318fe16bbc6f157a6f1948f974b03
root <qchalvondemersay@gmail.com>
2019-05-27 02:10:13 +0000

    change to MADDPG

--- buffer.py
+++ buffer.py
@@ -1,27 +1,46 @@
-from collections import deque
 import random
-from utilities import transpose_list
+import numpy as np
+import copy
+import torch
+from collections import namedtuple, deque
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class ReplayBuffer():
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, action_size, buffer_size, batch_size, seed):
+        """Initialize a ReplayBuffer object.
+        Params
+        ======
+            action_size (int): dimension of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): Random seed
+        """
+        random.seed(seed)
+        np.random.seed(seed)
+        self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
 
 
-class ReplayBuffer:
-    def __init__(self,size):
-        self.size = size
-        self.deque = deque(maxlen=self.size)
+    def add(self, state, action, reward, next_state, done):
+        """Add a new experience to memory."""
+        e = self.experience(state, action, reward, next_state, done)
+        self.memory.append(e)
 
-    def push(self,transition):
-        """push into the buffer"""
-        
-        input_to_buffer = transpose_list(transition)
-    
-        for item in input_to_buffer:
-            self.deque.append(item)
-
-    def sample(self, batchsize):
-        """sample from the buffer"""
-        samples = random.sample(self.deque, batchsize)
-
-        # transpose list of list
-        return transpose_list(samples)
+    def sample(self):
+        """Randomly sample a batch of experiences from memory."""
+        experiences = random.sample(self.memory, k=self.batch_size)
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        return (states, actions, rewards, next_states, dones)
 
     def __len__(self):
-        return len(self.deque)
+        """Return the current size of internal memory."""
+        return len(self.memory)
--- maddpg.py
+++ maddpg.py
@@ -1,120 +1,200 @@
-# main code that contains the neural network setup
-# policy + critic updates
-# see ddpg.py for other details in the network
+import numpy as np
+import random
+import copy
+from collections import namedtuple, deque
 
-from ddpg import DDPGAgent
+import model
+from ounoise import OUNoise
+from buffer import ReplayBuffer
+
 import torch
-from utilities import soft_update, transpose_to_tensor, transpose_list
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-device = 'cpu'
+import torch.nn.functional as F
+import torch.optim as optim
+
+BUFFER_SIZE = int(1e5)  # replay buffer size
+BATCH_SIZE = 512        # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_CRITIC = 1e-4        # learning rate of the critic
+WEIGHT_DECAY = 0        # L2 weight decay
+UPDATE_EVERY = 4        # frequence to update the network
+NOISE_START = 1.0       # parameters for noise
+NOISE_DECAY = 1.0
 
 
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-class MADDPG:
-    def __init__(self, discount_factor=0.95, tau=0.02):
-        super(MADDPG, self).__init__()
+class MADDPG():
+    """Agent that contains the two DDPG agents and shared replay buffer."""
 
-        # critic input = obs_full + actions = 14+2+2+2=20
-        self.maddpg_agent = [DDPGAgent(14, 16, 8, 2, 20, 32, 16), 
-                             DDPGAgent(14, 16, 8, 2, 20, 32, 16), 
-                             DDPGAgent(14, 16, 8, 2, 20, 32, 16)]
+    def __init__(self, action_size=2, n_agents=2, seed=0):
+        """
+        Params
+        ======
+            action_size (int): dimension of each action
+            seed (int): Random seed
+            n_agents (int): number of agents
+        """
         
-        self.discount_factor = discount_factor
-        self.tau = tau
-        self.iter = 0
+        self.n_agents = n_agents
+        self.t_step = 0
+        self.noise_on = True
 
-    def get_actors(self):
-        """get actors of all the agents in the MADDPG object"""
-        actors = [ddpg_agent.actor for ddpg_agent in self.maddpg_agent]
-        return actors
-
-    def get_target_actors(self):
-        """get target_actors of all the agents in the MADDPG object"""
-        target_actors = [ddpg_agent.target_actor for ddpg_agent in self.maddpg_agent]
-        return target_actors
-
-    def act(self, obs_all_agents, noise=0.0):
-        """get actions from all agents in the MADDPG object"""
-        actions = [agent.act(obs, noise) for agent, obs in zip(self.maddpg_agent, obs_all_agents)]
-        return actions
-
-    def target_act(self, obs_all_agents, noise=0.0):
-        """get target network actions from all the agents in the MADDPG object """
-        target_actions = [ddpg_agent.target_act(obs, noise) for ddpg_agent, obs in zip(self.maddpg_agent, obs_all_agents)]
-        return target_actions
-
-    def update(self, samples, agent_number, logger):
-        """update the critics and actors of all the agents """
-
-        # need to transpose each element of the samples
-        # to flip obs[parallel_agent][agent_number] to
-        # obs[agent_number][parallel_agent]
-        obs, obs_full, action, reward, next_obs, next_obs_full, done = map(transpose_to_tensor, samples)
-
-        obs_full = torch.stack(obs_full)
-        next_obs_full = torch.stack(next_obs_full)
+        # create two agents, each with their own actor and critic
+        models = [model.Actor_Critic_Models(n_agents=n_agents) for _ in range(n_agents)]
+        self.agents = [DDPG(i, models[i]) for i in range(n_agents)]
         
-        agent = self.maddpg_agent[agent_number]
-        agent.critic_optimizer.zero_grad()
+        # create shared replay buffer
+        self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, seed)
 
-        #critic loss = batch mean of (y- Q(s,a) from target network)^2
-        #y = reward of this timestep + discount * Q(st+1,at+1) from target network
-        target_actions = self.target_act(next_obs)
-        target_actions = torch.cat(target_actions, dim=1)
+    def step(self, all_states, all_actions, all_rewards, all_next_states, all_dones):
+        all_states = all_states.reshape(1, -1)  
+        all_next_states = all_next_states.reshape(1, -1)  
+        self.memory.add(all_states, all_actions, all_rewards, all_next_states, all_dones)
         
-        target_critic_input = torch.cat((next_obs_full.t(),target_actions), dim=1).to(device)
-        
-        with torch.no_grad():
-            q_next = agent.target_critic(target_critic_input)
-        
-        y = reward[agent_number].view(-1, 1) + self.discount_factor * q_next * (1 - done[agent_number].view(-1, 1))
-        action = torch.cat(action, dim=1)
-        critic_input = torch.cat((obs_full.t(), action), dim=1).to(device)
-        q = agent.critic(critic_input)
-
-        huber_loss = torch.nn.SmoothL1Loss()
-        critic_loss = huber_loss(q, y.detach())
-        critic_loss.backward()
-        #torch.nn.utils.clip_grad_norm_(agent.critic.parameters(), 0.5)
-        agent.critic_optimizer.step()
-
-        #update actor network using policy gradient
-        agent.actor_optimizer.zero_grad()
-        # make input to agent
-        # detach the other agents to save computation
-        # saves some time for computing derivative
-        q_input = [ self.maddpg_agent[i].actor(ob) if i == agent_number \
-                   else self.maddpg_agent[i].actor(ob).detach()
-                   for i, ob in enumerate(obs) ]
+        self.t_step = self.t_step + 1     
+        if self.t_step % UPDATE_EVERY == 0:
+            if len(self.memory) > BATCH_SIZE:
+                experiences = [self.memory.sample() for _ in range(self.n_agents)]
+                self.learn(experiences, GAMMA)
                 
-        q_input = torch.cat(q_input, dim=1)
-        # combine all the actions and observations for input to critic
-        # many of the obs are redundant, and obs[1] contains all useful information already
-        q_input2 = torch.cat((obs_full.t(), q_input), dim=1)
+
+    def act(self, all_states, add_noise=True):
+        # pass each agent's state from the environment and calculate its action
+        all_actions = []
+        for agent, state in zip(self.agents, all_states):
+            action = agent.act(state, add_noise=self.noise_on)
+            #self.noise_weight *= noise_decay
+            all_actions.append(action)
+        return np.array(all_actions).reshape(1, -1) # reshape 2x2 into 1x4 dim vector
+
+    def learn(self, experiences, gamma):
+        all_next_actions = []
+        all_actions = []
+        for i, agent in enumerate(self.agents):
+            states, _, _, next_states, _ = experiences[i]
+            agent_id = torch.tensor([i]).to(device)
+            # extract agent i's state and get action via actor network
+            state = states.reshape(-1, 2, 24).index_select(1, agent_id).squeeze(1)
+            action = agent.actor_local(state)
+            all_actions.append(action)
+            # extract agent i's next state and get action via target actor network
+            next_state = next_states.reshape(-1, 2, 24).index_select(1, agent_id).squeeze(1)
+            next_action = agent.actor_target(next_state)
+            all_next_actions.append(next_action)
+                       
+        for i, agent in enumerate(self.agents):
+            agent.learn(i, experiences[i], gamma, all_next_actions, all_actions)
+            
+    def save_agents(self):
+        for i, agent in enumerate(self.agents):
+            torch.save(agent.actor_local.state_dict(),  f"checkpoint_actor_agent_{i}.pth")
+            torch.save(agent.critic_local.state_dict(), f"checkpoint_critic_agent_{i}.pth")
+
+
+class DDPG():
+    """DDPG agent with own actor and critic."""
+
+    def __init__(self, agent_id, model, action_size=2, seed=0):
+        """Initialize an Agent object.
+        """
+        self.seed = random.seed(seed)
+        self.id = agent_id
+        self.action_size = action_size
+       
+        # Actor Network
+        self.actor_local = model.actor_local
+        self.actor_target = model.actor_target
+        self.actor_optimizer = optim.Adam(self.actor_local.parameters(), lr=LR_ACTOR)
         
-        # get the policy gradient
-        actor_loss = -agent.critic(q_input2).mean()
+        # Critic Network
+        self.critic_local = model.critic_local
+        self.critic_target = model.critic_target
+        self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+        
+        # Set weights for local and target actor, respectively, critic the same
+        self.hard_copy_weights(self.actor_target, self.actor_local)
+        self.hard_copy_weights(self.critic_target, self.critic_local)
+        
+        # Noise process
+        self.noise = OUNoise(action_size, seed)
+
+    def hard_copy_weights(self, target, source):
+        """ copy weights from source to target network (part of initialization)"""
+        for target_param, param in zip(target.parameters(), source.parameters()):
+            target_param.data.copy_(param.data)
+    
+    def act(self, state, noise_weight=1.0, add_noise=True):
+        """Returns actions for given state as per current policy."""
+        state = torch.from_numpy(state).float().to(device)
+        self.actor_local.eval()
+        with torch.no_grad():
+            action = self.actor_local(state).cpu().data.numpy()
+        self.actor_local.train()
+        if add_noise:
+            self.noise_val = self.noise.sample() * noise_weight
+            action += self.noise_val
+        return np.clip(action, -1, 1)
+
+    def reset(self):
+        self.noise.reset()
+
+    def learn(self, agent_id, experiences, gamma, all_next_actions, all_actions):
+        """Update policy and value parameters using given batch of experience tuples.
+        Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+        where:
+            actor_target(state) -> action
+            critic_target(state, action) -> Q-value
+        Params
+        ======
+            experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples 
+            gamma (float): discount factor
+        """
+
+        states, actions, rewards, next_states, dones = experiences
+
+        # ---------------------------- update critic ---------------------------- #
+        # get predicted next-state actions and Q values from target models
+        self.critic_optimizer.zero_grad()
+        agent_id = torch.tensor([agent_id]).to(device)
+        actions_next = torch.cat(all_next_actions, dim=1).to(device)
+        with torch.no_grad():
+            q_targets_next = self.critic_target(next_states, actions_next)
+        # compute Q targets for current states (y_i)
+        q_expected = self.critic_local(states, actions)
+        # q_targets = reward of this timestep + discount * Q(st+1,at+1) from target network
+        q_targets = rewards.index_select(1, agent_id) + (gamma * q_targets_next * (1 - dones.index_select(1, agent_id)))
+        # compute critic loss
+        critic_loss = F.mse_loss(q_expected, q_targets.detach())
+        # minimize loss
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        # ---------------------------- update actor ---------------------------- #
+        # compute actor loss
+        self.actor_optimizer.zero_grad()
+        # detach actions from other agents
+        actions_pred = [actions if i == self.id else actions.detach() for i, actions in enumerate(all_actions)]
+        actions_pred = torch.cat(actions_pred, dim=1).to(device)
+        actor_loss = -self.critic_local(states, actions_pred).mean()
+        # minimize loss
         actor_loss.backward()
-        #torch.nn.utils.clip_grad_norm_(agent.actor.parameters(),0.5)
-        agent.actor_optimizer.step()
+        self.actor_optimizer.step()
 
-        al = actor_loss.cpu().detach().item()
-        cl = critic_loss.cpu().detach().item()
-        logger.add_scalars('agent%i/losses' % agent_number,
-                           {'critic loss': cl,
-                            'actor_loss': al},
-                           self.iter)
-
-    def update_targets(self):
-        """soft update targets"""
-        self.iter += 1
-        for ddpg_agent in self.maddpg_agent:
-            soft_update(ddpg_agent.target_actor, ddpg_agent.actor, self.tau)
-            soft_update(ddpg_agent.target_critic, ddpg_agent.critic, self.tau)
-            
-            
-            
+        # ----------------------- update target networks ----------------------- #
+        self.soft_update(self.critic_local, self.critic_target, TAU)
+        self.soft_update(self.actor_local, self.actor_target, TAU)
 
 
-
-
+    def soft_update(self, local_model, target_model, tau):
+        """Soft update model parameters.
+        θ_target = τ*θ_local + (1 - τ)*θ_target
+        Params
+        ======
+            local_model (PyTorch model): weights will be copied from
+            target_model (PyTorch model): weights will be copied to
+            tau (float): interpolation parameter
+        """
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau*local_param.data + (1.0-tau)*target_param.data)
